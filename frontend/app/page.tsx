38d96b50f33d486a9dbcc343bad1e10506aa9bf7
s.umeda <syuhei19911212@gmail.com>
2024-09-26 20:00:22 +0900

    初期設定を行う

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-	return <div>テスト</div>;
+import { Box, HStack, Spinner, Stack, VStack } from "@chakra-ui/react";
+
+export default function HomePage() {
+	return (
+		<HStack h="100vh" w="100%" alignItems="stretch" bgColor="orange.100" p={4}>
+			<Box
+				w={{ base: "200px", md: "300px" }}
+				maxW="30%"
+				bgColor="orange.50"
+				borderRadius={10}
+			>
+				<h1>サイドバー</h1>
+			</Box>
+
+			<VStack flexGrow={1} spacing={0}>
+				<Box h="60px" w="100%" bgColor="orange.50" borderTopRadius={10}>
+					<h1>ヘッダー</h1>
+				</Box>
+				<Box flexGrow={1} w="100%" bgColor="orange.50" borderBottomRadius={10}>
+					<h1>メインコンテンツ</h1>
+				</Box>
+			</VStack>
+		</HStack>
+	);
 }
