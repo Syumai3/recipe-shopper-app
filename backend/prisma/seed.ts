ae740e1d39222619c15eda68904fd791e7705d3e
s.umeda <syuhei19911212@gmail.com>
2024-10-07 16:33:37 +0900

    seedデータを登録

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { units } from './units.js';
-import { ingredients } from './ingredients.js';
+import { units } from './seeds/units.js';
+import { ingredients } from './seeds/ingredients.js';
 const prisma = new PrismaClient();
 
 async function main() {
