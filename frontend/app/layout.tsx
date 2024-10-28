d1e0ed9fe24681343980636e69e9ac775e815b70
s.umeda <syuhei19911212@gmail.com>
2024-10-28 20:20:23 +0900

    セッション情報を管理する機能を追加

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,6 +5,7 @@ import ApolloChakraProvider from './ApolloChakraProvider';
 import Sidebar from './components/Sidebar';
 import { SessionProvider } from 'next-auth/react';
 import Header from './components/header';
+import NextAuthProvider from '@/src/providers/NextAuth';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,44 +22,51 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>
-        <ApolloChakraProvider>
-          <SessionProvider>
-            <Flex
-              h="100vh"
-              w="100%"
-              bgColor="orange.100"
-              alignItems="stretch"
-              p={4}
-            >
-              <Sidebar />
-              <Flex flexDirection="column" flexGrow={1} p={4} overflowY="auto">
-                <Box
-                  h="60px"
-                  w="100%"
-                  bgColor="orange.50"
-                  borderRadius={10}
-                  mb={4}
-                  display="flex"
-                  alignItems="center"
-                  padding={4}
-                  justifyContent="flex-end"
-                >
-                  <Header />
-                </Box>
-                <Box
+        <NextAuthProvider>
+          <ApolloChakraProvider>
+            <SessionProvider>
+              <Flex
+                h="100vh"
+                w="100%"
+                bgColor="orange.100"
+                alignItems="stretch"
+                p={4}
+              >
+                <Sidebar />
+                <Flex
+                  flexDirection="column"
                   flexGrow={1}
-                  w="100%"
-                  bgColor="orange.50"
-                  borderRadius={10}
                   p={4}
                   overflowY="auto"
                 >
-                  {children}
-                </Box>
+                  <Box
+                    h="60px"
+                    w="100%"
+                    bgColor="orange.50"
+                    borderRadius={10}
+                    mb={4}
+                    display="flex"
+                    alignItems="center"
+                    padding={4}
+                    justifyContent="flex-end"
+                  >
+                    <Header />
+                  </Box>
+                  <Box
+                    flexGrow={1}
+                    w="100%"
+                    bgColor="orange.50"
+                    borderRadius={10}
+                    p={4}
+                    overflowY="auto"
+                  >
+                    {children}
+                  </Box>
+                </Flex>
               </Flex>
-            </Flex>
-          </SessionProvider>
-        </ApolloChakraProvider>
+            </SessionProvider>
+          </ApolloChakraProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
